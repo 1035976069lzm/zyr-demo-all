36689a7b4f460fa2bfd83c21fb79cf81fc6f9f37
liuzunming <1035976069@qq.com>
2019-07-30 16:50:45 +0800

    first commit

---
+++ src/main/java/zyr/learn/demo/zyrdemoall/ZyrDemoAllApplication.java
@@ -0,0 +1,13 @@
+package zyr.learn.demo.zyrdemoall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyrDemoAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyrDemoAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zyr/learn/demo/zyrdemoall/ZyrDemoAllApplicationTests.java
@@ -0,0 +1,16 @@
+package zyr.learn.demo.zyrdemoall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZyrDemoAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
